a2bddfb906fca60940dcc8cbe4a4650b3d45642d
WalterVentura <walter.d.ventura@gmail.com>
2024-04-19 03:51:51 -0300

    Rewrite main using template

--- main.c
+++ main.c
@@ -1,21 +1,30 @@
+/*###############################################################################################*/
+
+// @file main.c
+
+/*                                                                                               */
+/*###############################################################################################*/
+
 /*
- *  Copyright 2016 - 2021 Benjamin Vedder	benjamin@vedder.se
+ * HW resources used:
  *
- *  This file is part of the VESC firmware.
+ * TIM1: mcpwm
+ * TIM2: mcpwm_foc
+ * TIM5: timer
+ * TIM8: mcpwm
+ * TIM3: servo_dec/Encoder (HW_R2)/pwm_servo
+ * TIM4: WS2811/WS2812 LEDs/Encoder (other HW)
  *
- *  The VESC firmware is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * DMA/stream	Device		Function
+ * 1, 2			I2C1		Nunchuk, temp on rev 4.5
+ * 1, 7			I2C1		Nunchuk, temp on rev 4.5
+ * 2, 4			ADC			mcpwm
  *
- *  The VESC firmware is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/*###############################################################################################*/
+/*     INCLUDES                                                                                  */
+/*###############################################################################################*/
 
 #pragma GCC push_options
 #pragma GCC optimize ("Os")
@@ -71,28 +80,229 @@
 #include "lispif.h"
 #endif
 
-/*
- * HW resources used:
- *
- * TIM1: mcpwm
- * TIM2: mcpwm_foc
- * TIM5: timer
- * TIM8: mcpwm
- * TIM3: servo_dec/Encoder (HW_R2)/pwm_servo
- * TIM4: WS2811/WS2812 LEDs/Encoder (other HW)
- *
- * DMA/stream	Device		Function
- * 1, 2			I2C1		Nunchuk, temp on rev 4.5
- * 1, 7			I2C1		Nunchuk, temp on rev 4.5
- * 2, 4			ADC			mcpwm
- *
- */
+/*###############################################################################################*/
+/*     DEFINES                                                                                   */
+/*###############################################################################################*/
 
-// Private variables
+/*###############################################################################################*/
+/*     TYPEDEFS                                                                                  */
+/*###############################################################################################*/
+
+/*###############################################################################################*/
+/*     PRIVATE FUNCTIONS PROTOTYPES                                                              */
+/*###############################################################################################*/
+
 static THD_WORKING_AREA(periodic_thread_wa, 256);
+
 static THD_WORKING_AREA(led_thread_wa, 256);
+
 static THD_WORKING_AREA(flash_integrity_check_thread_wa, 256);
+
+static THD_FUNCTION(flash_integrity_check_thread, arg);
+
+static THD_FUNCTION(led_thread, arg);
+
+static THD_FUNCTION(periodic_thread, arg);
+
+/*###############################################################################################*/
+/*     VARIABLES                                                                                 */
+/*###############################################################################################*/
+
 static volatile bool m_init_done = false;
+
+/*###############################################################################################*/
+/*     CONSTANTS                                                                                 */
+/*###############################################################################################*/
+
+/*###############################################################################################*/
+/*     PUBLIC FUNCTIONS                                                                          */
+/*###############################################################################################*/
+
+int main(void)
+{
+    halInit();
+    chSysInit();
+
+    // Initialize the enable pins here and disable them
+    // to avoid excessive current draw at boot because of
+    // floating pins.
+#ifdef HW_HAS_DRV8313
+    INIT_BR();
+#endif
+
+    HW_EARLY_INIT();
+
+#ifdef BOOT_OK_GPIO
+    palSetPadMode(BOOT_OK_GPIO, BOOT_OK_PIN, PAL_MODE_OUTPUT_PUSHPULL);
+    palClearPad(BOOT_OK_GPIO, BOOT_OK_PIN);
+#endif
+
+    chThdSleepMilliseconds(100);
+
+    mempools_init();
+    events_init();
+    timer_init(); // Initialize timer here to allow I2C in hw_init
+    hw_init_gpio();
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+
+    conf_general_init();
+
+    if(flash_helper_verify_flash_memory() == FAULT_CODE_FLASH_CORRUPTION)
+    {
+        // Loop here, it is not safe to run any code
+        while(1)
+        {
+            chThdSleepMilliseconds(100);
+            LED_RED_ON();
+            chThdSleepMilliseconds(75);
+            LED_RED_OFF();
+        }
+    }
+
+    ledpwm_init();
+    mc_interface_init();
+
+    commands_init();
+
+#if COMM_USE_USB
+    comm_usb_init();
+#endif
+
+    app_uartcomm_initialize();
+    app_configuration* appconf = mempools_alloc_appconf();
+    conf_general_read_app_configuration(appconf);
+    app_uartcomm_start(UART_PORT_BUILTIN);
+    app_uartcomm_start(UART_PORT_EXTRA_HEADER);
+    app_set_configuration(appconf);
+
+    // This reads the appconf, that must be initialized first.
+#if CAN_ENABLE
+    comm_can_init();
+#endif
+
+#ifdef HW_HAS_PERMANENT_NRF
+    conf_general_permanent_nrf_found = nrf_driver_init();
+
+    if(conf_general_permanent_nrf_found)
+    {
+        rfhelp_restart();
+    }
+    else
+    {
+        nrf_driver_stop();
+
+        // Set the nrf SPI pins to the general SPI interface so that
+        // an external NRF can be used with the NRF app.
+        spi_sw_change_pins(
+            HW_SPI_PORT_NSS, HW_SPI_PIN_NSS,
+            HW_SPI_PORT_SCK, HW_SPI_PIN_SCK,
+            HW_SPI_PORT_MOSI, HW_SPI_PIN_MOSI,
+            HW_SPI_PORT_MISO, HW_SPI_PIN_MISO);
+        HW_PERMANENT_NRF_FAILED_HOOK();
+    }
+
+#endif
+
+    // Threads
+    chThdCreateStatic(led_thread_wa, sizeof(led_thread_wa), NORMALPRIO, led_thread, NULL);
+    chThdCreateStatic(periodic_thread_wa, sizeof(periodic_thread_wa), NORMALPRIO, periodic_thread,
+                      NULL);
+    chThdCreateStatic(flash_integrity_check_thread_wa, sizeof(flash_integrity_check_thread_wa),
+                      LOWPRIO, flash_integrity_check_thread, NULL);
+
+    timeout_init();
+    timeout_configure(appconf->timeout_msec, appconf->timeout_brake_current, appconf->kill_sw_mode);
+
+#if HAS_BLACKMAGIC
+    bm_init();
+#endif
+
+    shutdown_init();
+
+    imu_reset_orientation();
+
+    chThdSleepMilliseconds(500);
+    m_init_done = true;
+
+#ifdef BOOT_OK_GPIO
+    palSetPad(BOOT_OK_GPIO, BOOT_OK_PIN);
+#endif
+
+#ifdef CAN_ENABLE
+
+    // Transmit a CAN boot-frame to notify other nodes on the bus about it.
+    if(appconf->can_mode == CAN_MODE_VESC)
+    {
+        comm_can_transmit_eid(
+            app_get_configuration()->controller_id | (CAN_PACKET_NOTIFY_BOOT << 8),
+            (uint8_t*) HW_NAME, (strlen(HW_NAME) <= CAN_FRAME_MAX_PL_SIZE) ?
+            strlen(HW_NAME) : CAN_FRAME_MAX_PL_SIZE);
+    }
+
+#endif
+
+    mempools_free_appconf(appconf);
+
+    for(;;)
+    {
+        chThdSleepMilliseconds(10);
+    }
+}
+
+// When assertions enabled halve PWM frequency. The control loop ISR runs 40% slower
+void assert_failed(uint8_t* file, uint32_t line)
+{
+    commands_printf("Wrong parameters value: file %s on line %d\r\n", file, line);
+    mc_interface_release_motor();
+
+    while(1)
+    {
+        chThdSleepMilliseconds(1);
+    }
+}
+
+bool main_init_done(void)
+{
+    return m_init_done;
+}
+
+uint32_t main_calc_hw_crc(void)
+{
+    uint32_t crc = 0;
+
+#ifdef QMLUI_SOURCE_HW
+    crc = crc32_with_init(data_qml_hw, DATA_QML_HW_SIZE, crc);
+#endif
+
+    for(int i = 0; i < conf_custom_cfg_num(); i++)
+    {
+        uint8_t* data = 0;
+        int len = conf_custom_get_cfg_xml(i, &data);
+
+        if(len > 0)
+        {
+            crc = crc32_with_init(data, len, crc);
+        }
+    }
+
+    if(flash_helper_code_size(CODE_IND_QML) > 0)
+    {
+        crc = crc32_with_init(
+            flash_helper_code_data(CODE_IND_QML),
+            flash_helper_code_size(CODE_IND_QML),
+            crc);
+    }
+
+    return crc;
+}
+
+
+
+
+/*###############################################################################################*/
+/*     PRIVATE FUNCTIONS                                                                         */
+/*###############################################################################################*/
 
 static THD_FUNCTION(flash_integrity_check_thread, arg)
 {
@@ -233,183 +443,9 @@ static THD_FUNCTION(periodic_thread, arg)
     }
 }
 
-// When assertions enabled halve PWM frequency. The control loop ISR runs 40% slower
-void assert_failed(uint8_t* file, uint32_t line)
-{
-    commands_printf("Wrong parameters value: file %s on line %d\r\n", file, line);
-    mc_interface_release_motor();
-
-    while(1)
-    {
-        chThdSleepMilliseconds(1);
-    }
-}
-
-bool main_init_done(void)
-{
-    return m_init_done;
-}
-
-uint32_t main_calc_hw_crc(void)
-{
-    uint32_t crc = 0;
-
-#ifdef QMLUI_SOURCE_HW
-    crc = crc32_with_init(data_qml_hw, DATA_QML_HW_SIZE, crc);
-#endif
-
-    for(int i = 0; i < conf_custom_cfg_num(); i++)
-    {
-        uint8_t* data = 0;
-        int len = conf_custom_get_cfg_xml(i, &data);
-
-        if(len > 0)
-        {
-            crc = crc32_with_init(data, len, crc);
-        }
-    }
-
-    if(flash_helper_code_size(CODE_IND_QML) > 0)
-    {
-        crc = crc32_with_init(
-            flash_helper_code_data(CODE_IND_QML),
-            flash_helper_code_size(CODE_IND_QML),
-            crc);
-    }
-
-    return crc;
-}
-
-int main(void)
-{
-    halInit();
-    chSysInit();
-
-    // Initialize the enable pins here and disable them
-    // to avoid excessive current draw at boot because of
-    // floating pins.
-#ifdef HW_HAS_DRV8313
-    INIT_BR();
-#endif
-
-    HW_EARLY_INIT();
-
-#ifdef BOOT_OK_GPIO
-    palSetPadMode(BOOT_OK_GPIO, BOOT_OK_PIN, PAL_MODE_OUTPUT_PUSHPULL);
-    palClearPad(BOOT_OK_GPIO, BOOT_OK_PIN);
-#endif
-
-    chThdSleepMilliseconds(100);
-
-    mempools_init();
-    events_init();
-    timer_init(); // Initialize timer here to allow I2C in hw_init
-    hw_init_gpio();
-    LED_RED_OFF();
-    LED_GREEN_OFF();
-
-    conf_general_init();
-
-    if(flash_helper_verify_flash_memory() == FAULT_CODE_FLASH_CORRUPTION)
-    {
-        // Loop here, it is not safe to run any code
-        while(1)
-        {
-            chThdSleepMilliseconds(100);
-            LED_RED_ON();
-            chThdSleepMilliseconds(75);
-            LED_RED_OFF();
-        }
-    }
-
-    ledpwm_init();
-    mc_interface_init();
-
-    commands_init();
-
-#if COMM_USE_USB
-    comm_usb_init();
-#endif
-
-    app_uartcomm_initialize();
-    app_configuration* appconf = mempools_alloc_appconf();
-    conf_general_read_app_configuration(appconf);
-    app_uartcomm_start(UART_PORT_BUILTIN);
-    app_uartcomm_start(UART_PORT_EXTRA_HEADER);
-    app_set_configuration(appconf);
-
-    // This reads the appconf, that must be initialized first.
-#if CAN_ENABLE
-    comm_can_init();
-#endif
-
-#ifdef HW_HAS_PERMANENT_NRF
-    conf_general_permanent_nrf_found = nrf_driver_init();
-
-    if(conf_general_permanent_nrf_found)
-    {
-        rfhelp_restart();
-    }
-    else
-    {
-        nrf_driver_stop();
-
-        // Set the nrf SPI pins to the general SPI interface so that
-        // an external NRF can be used with the NRF app.
-        spi_sw_change_pins(
-            HW_SPI_PORT_NSS, HW_SPI_PIN_NSS,
-            HW_SPI_PORT_SCK, HW_SPI_PIN_SCK,
-            HW_SPI_PORT_MOSI, HW_SPI_PIN_MOSI,
-            HW_SPI_PORT_MISO, HW_SPI_PIN_MISO);
-        HW_PERMANENT_NRF_FAILED_HOOK();
-    }
-
-#endif
-
-    // Threads
-    chThdCreateStatic(led_thread_wa, sizeof(led_thread_wa), NORMALPRIO, led_thread, NULL);
-    chThdCreateStatic(periodic_thread_wa, sizeof(periodic_thread_wa), NORMALPRIO, periodic_thread,
-                      NULL);
-    chThdCreateStatic(flash_integrity_check_thread_wa, sizeof(flash_integrity_check_thread_wa),
-                      LOWPRIO, flash_integrity_check_thread, NULL);
-
-    timeout_init();
-    timeout_configure(appconf->timeout_msec, appconf->timeout_brake_current, appconf->kill_sw_mode);
-
-#if HAS_BLACKMAGIC
-    bm_init();
-#endif
-
-    shutdown_init();
-
-    imu_reset_orientation();
-
-    chThdSleepMilliseconds(500);
-    m_init_done = true;
-
-#ifdef BOOT_OK_GPIO
-    palSetPad(BOOT_OK_GPIO, BOOT_OK_PIN);
-#endif
-
-#ifdef CAN_ENABLE
-
-    // Transmit a CAN boot-frame to notify other nodes on the bus about it.
-    if(appconf->can_mode == CAN_MODE_VESC)
-    {
-        comm_can_transmit_eid(
-            app_get_configuration()->controller_id | (CAN_PACKET_NOTIFY_BOOT << 8),
-            (uint8_t*) HW_NAME, (strlen(HW_NAME) <= CAN_FRAME_MAX_PL_SIZE) ?
-            strlen(HW_NAME) : CAN_FRAME_MAX_PL_SIZE);
-    }
-
-#endif
-
-    mempools_free_appconf(appconf);
-
-    for(;;)
-    {
-        chThdSleepMilliseconds(10);
-    }
-}
 
 #pragma GCC pop_options
+
+/*###############################################################################################*/
+/*     END OF FILE                                                                               */
+/*###############################################################################################*/
